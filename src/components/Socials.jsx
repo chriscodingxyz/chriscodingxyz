b5dad1af8c0f15dfd1e0ee3f75eff8152f78ec83
cherrydub <cherrydub@gmail.com>
2024-09-23 11:31:03 +0100

    style: updated minor look/feel of site with hover animations

--- src/components/Socials.jsx
+++ src/components/Socials.jsx
@@ -1,30 +1,34 @@
-import React from "react";
+import React from 'react'
 
-export default function Socials() {
+export default function Socials () {
   return (
-    <div className="socials">
+    <div className='socials'>
       {/* <i class="las la-grin-stars"></i>  */}
       Chris Wisniewski © <span> </span>
       {/* <i class="las la-thumbtack"></i> */}
-      <a title="GitHub" target="_blank" href="https://github.com/cherrydub">
-        <i class="lab la-github"></i>
+      <a
+        title='GitHub'
+        target='_blank'
+        href='https://github.com/chriscodingxyz'
+      >
+        <i class='lab la-github'></i>
       </a>
       <a
-        title="LinkedIn"
-        target="_blank"
-        href="https://www.linkedin.com/in/wisniewskichris/"
+        title='LinkedIn'
+        target='_blank'
+        href='https://www.linkedin.com/in/wisniewskichris/'
       >
-        <i class="lab la-linkedin"></i>
+        <i class='lab la-linkedin'></i>
       </a>
       <a
-        title="CV/Resume"
-        href="https://raw.githubusercontent.com/cherrydub/cherrydub/main/public/Chris_Wisniewski_CV.pdf"
+        title='CV/Resume'
+        href='https://docs.google.com/document/d/1OPZ7AvdwdWzd3o_vCGJ2zDj6sVUScl-3fwgepYFS2-g/edit?usp=sharing'
       >
-        <i class="las la-file-alt"></i>
+        <i class='las la-file-alt'></i>
       </a>
-      <a title="E-mail" href="mailto:chriscoding@icloud.com">
-        <i class="las la-envelope"></i>
+      <a title='E-mail' href='mailto:chriscoding@icloud.com'>
+        <i class='las la-envelope'></i>
       </a>
     </div>
-  );
+  )
 }
--- src/components/View.jsx
+++ src/components/View.jsx
@@ -1,94 +1,94 @@
-import React, { useEffect, useRef } from "react";
+import React, { useEffect, useRef } from 'react'
 
-export default function View() {
-  const portfolioRef = useRef(null);
-  const blogRef = useRef(null);
-  const windows98Ref = useRef(null);
+export default function View () {
+  const portfolioRef = useRef(null)
+  const blogRef = useRef(null)
+  const windows98Ref = useRef(null)
 
   useEffect(() => {
     // Function to trigger hover effect on a specific div after a delay
-    const triggerHover = (elementRef) => {
+    const triggerHover = elementRef => {
       if (elementRef.current) {
-        elementRef.current.classList.add("hovered");
+        elementRef.current.classList.add('hovered')
 
         // Set a timeout to remove the "hovered" class after a certain duration
         setTimeout(() => {
-          elementRef.current.classList.remove("hovered");
-        }, 1000); // Adjust the duration based on your design
+          elementRef.current.classList.remove('hovered')
+        }, 1000) // Adjust the duration based on your design
       }
-    };
+    }
 
     // Set initial delay
-    let delay = 3500; // Set the delay time in milliseconds
+    let delay = 3500 // Set the delay time in milliseconds
 
     // Trigger hover effect on each div after a delay
     setTimeout(() => {
-      triggerHover(portfolioRef);
-    }, delay);
+      triggerHover(portfolioRef)
+    }, delay)
 
-    delay += 1000; // Increase the delay for the next div
-
-    setTimeout(() => {
-      triggerHover(blogRef);
-    }, delay);
-
-    delay += 1000; // Increase the delay for the next div
+    delay += 1000 // Increase the delay for the next div
 
     setTimeout(() => {
-      triggerHover(windows98Ref);
-    }, delay);
-  }, []);
+      triggerHover(blogRef)
+    }, delay)
+
+    delay += 1000 // Increase the delay for the next div
+
+    setTimeout(() => {
+      triggerHover(windows98Ref)
+    }, delay)
+  }, [])
 
   return (
     <>
-      <div className="portfolio" ref={portfolioRef}>
+      <div className='portfolio' ref={portfolioRef}>
         <iframe
-          src="https://portfolio.cherrydub.com"
-          frameBorder="0"
-          height="100%"
-          width="100%"
-          title="portfolio"
+          src='https://portfolio.chriscoding.xyz'
+          frameBorder='0'
+          height='100%'
+          width='100%'
+          title='portfolio'
         ></iframe>
         <span>
-          <a target="_blank" href="https://portfolio.cherrydub.com">
+          <a target='_blank' href='https://portfolio.chriscoding.xyz'>
             <button>
-              <i class="las la-archive"></i>Portfolio
+              <i class='las la-archive'></i>Portfolio
             </button>
           </a>
         </span>
       </div>
-      <div className="blog" ref={blogRef}>
+      <div className='blog' ref={blogRef}>
         <iframe
-          src="https://blog.cherrydub.com"
-          frameBorder="0"
-          height="100%"
-          width="100%"
-          title="blog"
+          src='https://chriswiz.vercel.app'
+          frameBorder='0'
+          height='100%'
+          width='100%'
+          title='blog'
         ></iframe>
         <span>
-          <a target="_blank" href="https://blog.cherrydub.com">
+          <a target='_blank' href='https://chriswiz.vercel.app'>
             <button>
-              <i class="las la-file-alt"></i>Blog
+              <i class='las la-file-alt'></i>Minimal
             </button>
           </a>
         </span>
       </div>
-      <div className="windows98" ref={windows98Ref}>
+      <div className='windows98' ref={windows98Ref}>
         <iframe
-          src="https://98.cherrydub.com"
-          frameBorder="0"
-          height="100%"
-          width="100%"
-          title="windows 98"
+          src='https://98.chriscoding.xyz'
+          frameBorder='0'
+          height='100%'
+          width='100%'
+          title='windows 98'
         ></iframe>
-        <span className="spanbox">
-          <a target="_blank" href="https://98.cherrydub.com">
+        <span className='spanbox'>
+          <a target='_blank' href='https://98.chriscoding.xyz'>
             <button>
-              <i class="lab la-windows"></i>98
+              <i class='lab la-windows'></i>98
             </button>
           </a>
         </span>
       </div>
     </>
-  );
+  )
 }
